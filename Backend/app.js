272bb4b65bf58b0b73865ee1c4aebb3ffb50ec00
joker1327 <jchristian2095@conestogac.on.ca>
2025-02-12 01:46:27 -0500

    changes

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+connectToDb();
+
+// for development purpose only accepts requests from all websites but will change when in production
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/user', userRoutes);
+
+module.exports = app;
