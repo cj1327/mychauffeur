99dff0a173c07b75beaa850410ac18e51c74e2b6
joker1327 <jchristian2095@conestogac.on.ca>
2025-02-13 00:41:50 -0500

    Frontend data

---
+++ frontend/src/pages/CaptainLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CaptainLogin = () => {
+    return (
+        <div>
+        <h1>CaptainLogin</h1>
+        </div>
+    ) 
+}
+
+export default CaptainLogin;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.{js,jsx,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
