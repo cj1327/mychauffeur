639676533fb459c80d2d15bc1a58179d6c400632
joker1327 <jchristian2095@conestogac.on.ca>
2025-02-20 01:31:42 -0500

    frontend mostly done

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const UserLogin = () => {
@@ -9,48 +9,52 @@ const UserLogin = () => {
     const submitHandler = (e) => {
         e.preventDefault();
         setUserData({
-            email:email, 
-            password:password
+            email: email, 
+            password: password
         });
-        // console.log(userData);
         setEmail('');
         setPassword('');
     }
 
+    // useEffect(() => {
+    //     console.log(userData);
+    // }, [userData]);
+
     return (
         <div className="p-7 h-screen flex flex-col justify-between">
             <div>
-            <img className='w-20 mb-10' src="/MyChauffeur_Logo.png" alt="logo" />
-            <form onSubmit={(e) => {
-                submitHandler(e);
-            }}>
-                <h3 className="text-lg font-medium mb-2">What's your email</h3>
-                <input 
-                required
-                value={email}
-                onChange={(e) =>{ 
-                    setEmail(e.target.value)
-                }}
-                className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-                type="email" 
-                placeholder="email@example.com"
-                />
+                <img className='w-20 mb-10' src="/MyChauffeur_Logo.png" alt="logo" />
+                <form onSubmit={(e) => {
+                    submitHandler(e);
+                }}>
+                    <h3 className="text-lg font-medium mb-2">What's your email</h3>
+                    <input 
+                        required
+                        value={email}
+                        onChange={(e) =>{ 
+                            setEmail(e.target.value)
+                        }}
+                        className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+                        type="email" 
+                        placeholder="email@example.com"
+                    />
 
-                <h3 className="text-lg font-medium mb-2">Enter Password</h3>
-                <input 
-                required 
-                value={password}
-                onChange={(e) =>{ 
-                    setPassword(e.target.value)
-                }}
-                className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-                type="password" 
-                placeholder="password"/>
+                    <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+                    <input 
+                        required 
+                        value={password}
+                        onChange={(e) =>{ 
+                            setPassword(e.target.value)
+                        }}
+                        className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+                        type="password" 
+                        placeholder="password"
+                    />
 
-                <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">Login</button>
+                    <button className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base">Login</button>
 
-                <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
-            </form>
+                    <p className='text-center'>New here? <Link to='/signup' className='text-blue-600'>Create new Account</Link></p>
+                </form>
             </div>
             <div>
                 <Link to='/captain-login' className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded px-4 py-2 w-full text-lg placeholder:text-base">Sign in as Captain</Link>
